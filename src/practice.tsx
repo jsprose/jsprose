a617e8e122bde99841a93285f2b5a611c97d7d79
Peter Radko <cmtvpetya@gmail.com>
2025-08-31 19:38:06 +0300

    Remove unused "testing" files

--- src/practice.tsx
+++
@@ -1,7 +0,0 @@
-import { tPifagor } from './article';
-import { Blocks } from './default';
-import { defineDocument } from './document';
-
-export const practice = defineDocument({
-    blocks: () => <Blocks>{tPifagor}</Blocks>,
-});
